54bba72de91ea33fd5c7ef55c6ed5533c66c4c43
pam-29 <pamelaarijaona@gmail.com>
2025-03-28 00:26:55 +0100

    start responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/Logo.svg'
 import '../style/Header.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { RiCloseFill } from 'react-icons/ri'
 
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+
     return (
             <section className="h-wrapper">
                 <div className="h-container">
@@ -18,6 +23,25 @@ function Header() {
                         <button>
                         <a href="">Contact</a>
                         </button>
+                    </div>
+
+                    <div>
+                        <div className='hambergerMenu'>
+                            {isMenuOpen ? 
+                                (<RiCloseFill size={40} oonClick={() => setIsMenuOpen(!isMenuOpen)}/>) : 
+                                (<GiHamburgerMenu size={40} onClick={() => setIsMenuOpen(!isMenuOpen)}/>)
+                            } 
+                        </div>
+
+                        {isMenuOpen &&(
+
+                            <div className="h-menu-mobile">
+                                <a href="#accueil">Accueil</a>
+                                <a href="#hotels">Hôtels</a>
+                                <a href="#value">Nos valeurs</a>
+                                <a href="#contact">Nous joindre</a>
+                            </div>
+                        )}
                     </div>
                 </div>
             </section>
